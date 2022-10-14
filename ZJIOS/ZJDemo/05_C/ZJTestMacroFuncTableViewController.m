9d04ed0c64d6289de4dfa4a676ab19d3309dabfe
zengjian2 <jianzengj@isoftstone.com>
2022-10-14 18:11:21 +0800

    a

--- ZJIOS/ZJDemo/05_C/ZJTestMacroFuncTableViewController.m
+++ ZJIOS/ZJDemo/05_C/ZJTestMacroFuncTableViewController.m
@@ -11,23 +11,6 @@
 
 @end
 
-#define RECORD_TIME(NAME) double _##NAME = [NSDate date].timeIntervalSince1970;
-
-#define TTF_STRINGIZE(x) #x
-#define TTF_STRINGIZE2(x) TTF_STRINGIZE(x)
-#define TTF_SHADER_STRING(text) @ TTF_STRINGIZE2(text)
-
-static NSString *const CAMREA_RESIZE_VERTEX = TTF_SHADER_STRING
-(
-attribute vec4 position;
-attribute vec4 inputTextureCoordinate;
-varying vec2 textureCoordinate;
-void main(){
-    textureCoordinate = inputTextureCoordinate.xy;
-    gl_Position = position;
-}
-);
-
 @implementation ZJTestMacroFuncTableViewController
 
 - (void)viewDidLoad {
@@ -38,7 +21,7 @@ void main(){
 }
 
 - (void)initAry {
-    self.cellTitles = @[@"宏变量名转换符:#", @"宏连接符:##"];
+    self.cellTitles = @[@"宏变量名转换符:#", @"宏连接符_1:##", @"宏连接符_2:##", @"宏替换符:__VA_ARGS__", @"宏替换符:##__VA_ARGS__", @"attribute vec4"];
 }
 
 - (void)initSetting {
@@ -55,33 +38,82 @@ void main(){
 }
 
 // '##'运算符:可以用于宏函数的替换部分。这个运算符把两个语言符号组合成单个语言符号，为宏扩展提供了一种连接实际变元的手段
-#define Func_Area(a) printf("the square of "#a" is %d.\n", area_##a)
+// ## 是宏连接符，作变量链接
+#define Func_Area(n) printf("the square of "#n" is %d.\n", area_##n)
 
 - (void)test1 {
-    int a = 30;
-    int area_a = a*a;
-    Func_Area(a);   // 输出: the square of a is 900.
+    int m = 30;
+    int area_m = m*m;
+    Func_Area(m);   // 输出: the square of m is 900.
 }
 
 /*
- 分析》：## 是宏连接符，作变量链接，Func(a)里面有b##a，也就是说直接连接成b‘a’，Func3(m)对应bm，由于bm在main里面有定义，所以可以打印出来。
- ##__VA_ARGS__这里的‘##’有特殊作用，
-   __VA_ARGS__是可变参数宏，用法如下：
-
-  #define Debug(...) printf(__VA_ARGS__)
-   使用的时候只需要：
-  Debug("Y = %d\n", y);
-
- 此时编译器会自动替换成printf("Y = %d\n", y");
- 对于##__VA_ARGS__的‘##’符号的用法，
- 例如：#define debug(format, ...) fprintf (stderr, format, ## __VA_ARGS__)
- 假如可变参数宏为空的时候，”“##”的作用就是让编译器忽略前面一个逗号，不然编译器会报错。
+ ##：用于将带参数的宏定义中将两个子串(token)联接起来，
+ 从而形成一个新的子串；但它不可以是第一个或者最后一个子串。所谓的子串(token)就是指编译器能够识别的最小语法单元；
  */
+#define LOG(x) log##x()
+
+void logA(void) {
+    printf("log func A.\n");
+}
+
+void logB(void) {
+    printf("log func B.\n");
+}
 
 - (void)test2 {
+    LOG(A); // 输出:log func A.
+    getchar();
+}
+
+/*
+ __VA_ARGS__:用于在宏替换部分中，表示可变参数列表；类似函数的可变参数中的省略号；
+ ##__VA_ARGS__ 宏前面加上##的作用在于，当可变参数的个数为0时，这里的##起到把前面多余的","去掉的作用
+ */
+
+#define debug_log_func1(format, ...) printf(format, __VA_ARGS__)
+#define debug_log_func2(format, ...) printf(format, ##__VA_ARGS__)
+- (void)test3 {
+    debug_log_func1("%s:%d\r\n", "debug", 100);
+}
+
+- (void)test4 {
+    /*
+     __VA_ARGS__会替换为与省略号匹配的所有参数，同时会将省略号前面的一个逗号带上，既debug_log_1("debug")，会拓展成printf("debug",)末尾多了个逗号
+     debug_log_func1("debug");  // 会报错
+     */
+    debug_log_func2("debug");   //
+}
+
+// '##': 不可以是第一个或者最后一个子串,所以(_##NAME)加了下划线,去掉下划线会报错
+#define RECORD_TIME(NAME) double _##NAME = [NSDate date].timeIntervalSince1970;
+
+#define TTF_STRINGIZE(x) #x
+//#define TTF_STRINGIZE2(x) TTF_STRINGIZE(x)
+#define TTF_STRINGIZE2(x) #x
+#define TTF_SHADER_STRING(text) @TTF_STRINGIZE2(text)
+
+//static NSString *const CAMREA_RESIZE_VERTEX = TTF_SHADER_STRING
+//(
+//attribute vec4 position;
+//attribute vec4 inputTextureCoordinate;
+//varying vec2 textureCoordinate;
+//void main(){
+//    textureCoordinate = inputTextureCoordinate.xy;
+//    gl_Position = position;
+//}
+//);
+
+static NSString *const CAMREA_RESIZE_VERTEX = TTF_SHADER_STRING(abc);
+/*
+ 去掉'@'的宏定义
+#define TTF_SHADER_STRING(text) TTF_STRINGIZE2(text)
+static NSString *const CAMREA_RESIZE_VERTEX = @TTF_SHADER_STRING(abc);
+*/
+
+- (void)test5 {
     RECORD_TIME(began);
     NSLog(@"_began = %f", _began);
-    
     NSString *str = CAMREA_RESIZE_VERTEX;
     NSLog(@"str = %@", str);
 }
@@ -98,6 +130,7 @@ void main(){
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:SystemTableViewCell];
     }
     cell.textLabel.text = self.cellTitles[indexPath.row];
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     
     return cell;
 }
@@ -108,7 +141,7 @@ void main(){
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
     SEL s = NSSelectorFromString([NSString stringWithFormat:@"test%zd", indexPath.row]);
-    [self performSelector:@selector(s)];
+    [self performSelector:s];
 }
 
 /*
