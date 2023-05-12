2571c64389cb400c5fb439fa2cf619b39145aa6c
zjmdyd <zjmdyd@163.com>
2023-05-12 22:51:10 +0800

    a

---
+++ ZJIOS/ZJDemo/ZJBaseTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  ZJBaseTableViewController.h
+//  ZJIOS
+//
+//  Created by Zengjian on 2023/5/12.
+//
+
+#import "ZJTableViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJBaseTableViewController : ZJTableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
