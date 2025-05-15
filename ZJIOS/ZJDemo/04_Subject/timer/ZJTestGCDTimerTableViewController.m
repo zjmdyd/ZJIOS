cdf79d5ddcd07c8f5d287b26efc4488b3f1ce701
zjmdyd <zjmdyd@163.com>
2025-05-15 17:12:17 +0800

    timer

--- ZJIOS/ZJDemo/04_Subject/timer/ZJTestGCDTimerTableViewController.m
+++ ZJIOS/ZJDemo/04_Subject/timer/ZJTestGCDTimerTableViewController.m
@@ -18,7 +18,7 @@ dispatch_source_t dis_timer;
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self test1];
+    [self test3];
 }
 
 /*
@@ -30,25 +30,56 @@ dispatch_source_t dis_timer;
  
  #define NSEC_PER_SEC 1000000000ull     // 1秒    10亿纳秒      1秒=10亿纳秒
  #define NSEC_PER_MSEC 1000000ull       // 1毫秒  100万纳秒      1毫秒=100万纳秒
- #define USEC_PER_SEC 1000000ull        // 1毫秒  100万微秒      1秒=100万微秒
+ #define USEC_PER_SEC 1000000ull        // 1秒    100万微秒      1秒=100万微秒
  #define NSEC_PER_USEC 1000ull          // 1微秒  1000纳秒      1微秒=1000纳秒
  */
 - (void)test0 {
-    bool a = (NSEC_PER_SEC == NSEC_PER_MSEC*1000);  // 秒<--毫秒(100万纳秒)
+    bool a = (NSEC_PER_SEC == NSEC_PER_MSEC*1000);      // 10^9 纳秒/秒 <-- 10^6 纳秒/毫秒
     NSLog(@"a = %d", a);    // a = 1
     
-    bool b = (NSEC_PER_SEC == USEC_PER_SEC*1000);   // 秒<--毫秒(100万微秒)
+    bool b = (NSEC_PER_SEC == USEC_PER_SEC*1000);       // 10^9 纳秒/秒 <-- 10^6 微秒/秒
     NSLog(@"b = %d", b);    // b = 1
     
-    bool c = (NSEC_PER_SEC == NSEC_PER_USEC*1000*1000); // 10亿纳秒(每秒)<--1000纳秒(微秒)
+    bool c = (NSEC_PER_SEC == NSEC_PER_USEC*1000*1000); // 10^9 纳秒/秒 <-- 10^3 纳秒/微秒
     NSLog(@"c = %d", c);    // c = 1
     
-    bool d = (NSEC_PER_MSEC == USEC_PER_SEC);       // 毫秒(100万纳秒)<--毫秒(100万微秒)
+    bool d = (NSEC_PER_MSEC == USEC_PER_SEC);           // 10^6 纳秒/毫秒 <-- 10^6 微秒/秒
     NSLog(@"d = %d", d);    // d = 1
 }
 
 /*
  GCD定时器完全不受runloop影响，也就是说不会受到手势和UI刷新影响。所以它比NSTimer 更加准确
+ ‌(1) type：事件类型‌
+ 支持以下系统级事件监听：
+ ‌‌DISPATCH_SOURCE_TYPE_TIMER‌：定时器（高精度计时）
+ ‌‌DISPATCH_SOURCE_TYPE_SIGNAL‌：UNIX 信号（如 SIGTERM）
+ ‌‌DISPATCH_SOURCE_TYPE_READ/WRITE‌：文件或 Socket 的 I/O 可读/可写状态
+ ‌‌DISPATCH_SOURCE_TYPE_VNODE‌：文件系统事件（删除、重命名等）
+ ‌‌DISPATCH_SOURCE_TYPE_PROC‌：进程状态变化（如退出）
+ ‌‌DISPATCH_SOURCE_TYPE_DATA_ADD/OR‌：自定义事件（手动触发）
+ 
+ ‌‌(2) handle：事件句柄‌
+ 根据 type 不同，传入对应的系统资源标识符：
+ ‌‌文件描述符‌（DISPATCH_SOURCE_TYPE_READ）：传入 int fd
+ ‌‌进程ID‌（DISPATCH_SOURCE_TYPE_PROC）：传入 pid_t
+ ‌‌信号值‌（DISPATCH_SOURCE_TYPE_SIGNAL）：传入 int signo
+ ‌‌定时器‌（DISPATCH_SOURCE_TYPE_TIMER）：传 0（无实际句柄）
+ 
+ ‌‌(3) mask：事件掩码‌
+ 进一步细化监听条件47：
+ ‌‌DISPATCH_SOURCE_TYPE_PROC‌：可监听 EXIT、FORK 等子事件
+ ‌‌DISPATCH_SOURCE_TYPE_VNODE‌：可监听 DELETE、WRITE 等文件操作
+ ‌‌其他类型‌：通常传 0（无特殊掩码需求）
+ ‌‌(4) queue：回调队列‌
+ 事件触发时，回调 block 执行的队列（主队列或自定义队列)
+ ‌(1) 优势‌
+ ‌‌高精度‌：不受 RunLoop 影响（如 NSTimer 在滑动时可能失效）
+ ‌‌低开销‌：直接监听系统内核事件（基于 kqueue 或 mach port）
+ ‌‌线程安全‌：自动管理事件与队列的同步6
+ ‌‌(2) 注意事项‌
+ ‌‌资源释放‌：必须调用 dispatch_source_cancel 并实现 cancel_handler 释放资源（如关闭文件描述符）
+ ‌‌生命周期管理‌：需强引用 dispatch_source_t 对象，避免提前释放26
+ ‌‌自定义事件‌：通过 dispatch_source_merge_data 触发 DATA_ADD/OR 类型事
  */
 - (void)test1 {
     // 获得队列
@@ -57,8 +88,8 @@ dispatch_source_t dis_timer;
     // 创建一个定时器
     dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
     
-    // 开始时间
-    dispatch_time_t start = dispatch_time(DISPATCH_TIME_NOW, NSEC_PER_SEC);
+    // 开始时间, 多少时间后开始
+    dispatch_time_t start_time = dispatch_time(DISPATCH_TIME_NOW, NSEC_PER_SEC);
     
     uint64_t interval = NSEC_PER_SEC;
     /* 设置定时器
@@ -67,27 +98,24 @@ dispatch_source_t dis_timer;
      * param4: 可接受的误差时间，设置0即不允许出现误差,系统只会尽可能满足这个需求，无法完全精确
      * Tips: 单位均为纳秒
      */
-    dispatch_source_set_timer(timer, start, interval, 0);
-    
-    /* 设置定时器任务
-     * 可以通过block方式
-     * 也可以通过C函数方式
-     */
+    dispatch_source_set_timer(timer, start_time, interval, 0);
+
+    __weak typeof(self) weakSelf = self;
+    static int count = 0;
     dispatch_source_set_event_handler(timer, ^{
-        static int count = 0;
         count++;
         NSLog(@"currentThread = %@, count_1 = %d", [NSThread currentThread], count);
-        if (count == 8) {
-            NSLog(@"执行cancel");
-            dispatch_cancel(timer);
-            
-            // 3秒后重启timer , cancel后再重启后会crash
-//            dispatch_time_t time = dispatch_time(DISPATCH_TIME_NOW, 3ull * NSEC_PER_SEC);
-//            dispatch_after(time, queue, ^{
-//                dispatch_resume(timer);
-//            });
-        }else {
-            NSLog(@"count不等于8");
+        // 在kCFRunLoopDefaultMode和UITrackingRunLoopMode中切换
+        // 滑动事件不会干扰定时器
+        NSLog(@"self = %@, currentMode = %@", weakSelf, [NSRunLoop currentRunLoop].currentMode);
+        if (!weakSelf) {
+            NSLog(@"self销毁,主动调用dispatch_cancel");
+            dispatch_source_cancel(timer);
+        }
+        if (count == 15) {
+            NSLog(@"目标任务结束,主动调用dispatch_cancel");
+            dispatch_source_cancel(timer);  // 或者调用dispatch_cancel(timer);
+            // dispatch_resume(timer); 重启timer , cancel后再重启后会crash
         }
     });
     
@@ -96,82 +124,100 @@ dispatch_source_t dis_timer;
     
     dispatch_source_set_cancel_handler(timer, ^{
         NSLog(@"timer执行了cancel事件");
+        count = 0;
     });
 }
 
+/*
+ currentMode = (null); 常见原因
+ 不在主线程上运行：NSRunLoop 主要与主线程（即 UI 线程）相关联。如果你的代码不在主线程上运行，那么 currentMode 可能会返回 nil
+ */
 - (void)test2 {
     // 获得队列
-    dispatch_queue_t queue = dispatch_get_global_queue(0, 0);
-    
+//    dispatch_queue_t queue = dispatch_get_global_queue(QOS_CLASS_USER_INTERACTIVE, 0);
+//    并行: DISPATCH_QUEUE_CONCURRENT‌ 串行:DISPATCH_QUEUE_SERIAL
+    dispatch_queue_t queue = dispatch_queue_create("com.zj.queue", DISPATCH_QUEUE_SERIAL);
+
     // 创建一个定时器
     dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
     
-    // 开始时间
-    dispatch_time_t start = dispatch_time(DISPATCH_TIME_NOW, NSEC_PER_SEC);
+    // 开始时间, 多少时间后开始
+    dispatch_time_t start_time = dispatch_time(DISPATCH_TIME_NOW, NSEC_PER_SEC);
     
     uint64_t interval = NSEC_PER_SEC;
     /* 设置定时器
      * param2: 任务开始时间
      * param3: 任务的间隔
      * param4: 可接受的误差时间，设置0即不允许出现误差,系统只会尽可能满足这个需求，无法完全精确
-     * Tips: 单位均为纳秒
      */
-    dispatch_source_set_timer(timer, start, interval, 0);
+    dispatch_source_set_timer(timer, start_time, interval, 0);
     
-    /* 设置定时器任务
-     * 可以通过block方式
-     * 也可以通过C函数方式
-     */
+    __weak typeof(self) weakSelf = self;
+    static int count = 0;
+    NSLog(@"count = %p", &count);   // 0x10af2df18
     dispatch_source_set_event_handler(timer, ^{
-        static int count = 0;
-        count++;
+        @synchronized (self) {
+            count++;
+        }
         NSLog(@"currentThread = %@, count_2 = %d", [NSThread currentThread], count);
         
-        if(count == 8) {
+        // currentMode = (null),因为不在主线程上运行
+        // 滑动事件不会干扰定时器
+        NSLog(@"self = %@, currentMode = %@", weakSelf, [NSRunLoop currentRunLoop].currentMode);
+        if (!weakSelf) {
+            NSLog(@"self销毁,主动调用dispatch_cancel");
+            dispatch_source_cancel(timer);
+        }
+        if (count == 15) {
+            NSLog(@"目标任务结束,主动调用dispatch_cancel");
+            dispatch_source_cancel(timer);  // 或者调用dispatch_cancel(timer);
+        }
+        
+        if(count == 5) {
             NSLog(@"执行suspend");
-            //终止定时器(如果没有终止方法,则定时器不会启动)
+            // 暂停定时器
             dispatch_suspend(timer);    // suspend可重新resume
             
-            // 3秒后重启timer
+            // 2秒后重启timer
             // dispatch_after函数并不是在指定时间后执行处理，而是在指定时间追加到Dispatch Queue
-            dispatch_time_t time = dispatch_time(DISPATCH_TIME_NOW, 3ull * NSEC_PER_SEC);
+            dispatch_time_t time = dispatch_time(DISPATCH_TIME_NOW, 2ull * NSEC_PER_SEC);
             dispatch_after(time, queue, ^{
                 dispatch_resume(timer);
             });
-        }else {
-            NSLog(@"count不等于8");
-            if (count == 16) {
-                dispatch_suspend(timer);
-            }
         }
     });
     
     //启动任务，GCD计时器创建后需要手动启动
     dispatch_resume(timer);
+    dispatch_source_set_cancel_handler(timer, ^{
+        @synchronized (self) {
+            count = 0;
+        }
+        NSLog(@"currentThread = %@, count_2 = %d", [NSThread currentThread], count);
+        NSLog(@"timer执行了cancel事件");
+    });
 }
 
 - (void)test3 {
     // 获得队列
-    dispatch_queue_t queue = dispatch_get_global_queue(0, 0);
-    
+//    dispatch_queue_t queue = dispatch_get_global_queue(0, 0);
+    dispatch_queue_t queue = dispatch_queue_create("com.zj.queue", DISPATCH_QUEUE_SERIAL);
+
     // 创建一个定时器
     dis_timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
     
-    // 开始时间
-    dispatch_time_t start = dispatch_time(DISPATCH_TIME_NOW, NSEC_PER_SEC);
+    // 开始时间     多少时间后开始
+    dispatch_time_t start_time = dispatch_time(DISPATCH_TIME_NOW, NSEC_PER_SEC);
     
     uint64_t interval = NSEC_PER_SEC;
     /* 设置定时器
      * param2: 任务开始时间
      * param3: 任务的间隔
      * param4: 可接受的误差时间，设置0即不允许出现误差,系统只会尽可能满足这个需求，无法完全精确
-     * Tips: 单位均为纳秒
      */
-    dispatch_source_set_timer(dis_timer, start, interval, 0);
+    dispatch_source_set_timer(dis_timer, start_time, interval, 0);
     
-    /* 设置定时器任务
-     * 可以通过block方式
-     * 也可以通过C函数方式
+    /* 设置定时器任务: 可以通过block方式 也可以通过C函数方式
      */
     dispatch_source_set_event_handler_f(dis_timer, (void *)timerEvent);
     
@@ -181,14 +227,37 @@ dispatch_source_t dis_timer;
 
 void timerEvent(void) {
     static int count = 0;
+//    NSLock *lock = [[NSLock alloc] init];
+//    [lock lock];
     count++;
+//    [lock unlock];
     NSLog(@"currentThread = %@, count = %d", [NSThread currentThread], count);
-    NSLog(@"%@", [NSThread currentThread]);
-    
-    if(count == 8) {
-        //终止定时器(如果没有终止方法,则定时器不会启动)
-        dispatch_suspend(dis_timer);
+
+    //终止定时器
+    if (count == 15) {
+        NSLog(@"目标任务结束,主动调用dispatch_cancel");
+        dispatch_source_cancel(dis_timer);  // 或者调用dispatch_cancel(timer);
     }
+    
+    if(count == 5) {
+        NSLog(@"执行suspend");
+        // 暂停定时器
+        dispatch_suspend(dis_timer);    // suspend可重新resume
+        
+        // 2秒后重启timer
+        // dispatch_after函数并不是在指定时间后执行处理，而是在指定时间追加到Dispatch Queue
+        dispatch_time_t time = dispatch_time(DISPATCH_TIME_NOW, 2ull * NSEC_PER_SEC);
+        dispatch_queue_t queue = dispatch_queue_create("com.zj.queue", DISPATCH_QUEUE_SERIAL);
+        dispatch_after(time, queue, ^{
+            dispatch_resume(dis_timer);
+        });
+    }
+    
+    dispatch_source_set_cancel_handler(dis_timer, ^{
+        count = 0;
+        NSLog(@"timer执行了cancel事件");
+        NSLog(@"currentThread = %@, count_2 = %d", [NSThread currentThread], count);
+    });
 }
 
 #pragma mark - UITableViewDataSource
@@ -212,10 +281,6 @@ void timerEvent(void) {
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
-}
-
-- (void)dealloc {
-    NSLog(@"%s", __func__);
 }
 
 /*
--- ZJIOS/ZJDemo/04_Subject/timer/ZJTestScrollTimerTableViewController.m
+++ ZJIOS/ZJDemo/04_Subject/timer/ZJTestScrollTimerTableViewController.m
@@ -20,14 +20,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self test3];
+    [self test2];
 }
 
 /*
  timer不准确: NSTimer 的runloop类型是NSDefaultRunloopMode 主线程中， 界面的刷新也在主线程中，
- UIScrollview滑动的过程中是在UITrackingRunLoopMode中，当我们在手指滑动过程中，系统会将NSDefaultRunloopMode 更改为NSTrackingRunloopMode，所以会出现NSTimer短暂暂停的现象
+ UIScrollview滑动的过程中是在UITrackingRunLoopMode中，当我们在手指滑动过程中，系统会将NSDefaultRunloopMode 更改为NSTrackingRunloopMode，所以会出现NSTimer暂停的现象
  */
-- (void)test1 {
+- (void)test0 {
     __weak typeof(self) weak_self = self;
     self.timer = [NSTimer zj_scheduledTimerWithTimeInterval:1 repeats:YES block:^(NSTimer * _Nonnull timer) {
         weak_self.index++;
@@ -40,7 +40,7 @@
  解决timer不准确: 设置成NSRunLoopCommonModes
  滑动scrollView是在UITrackingRunLoopMode, 设置成NSRunLoopCommonModes时,不会出现timer暂停现象
  */
-- (void)test2 {
+- (void)test1 {
     __weak typeof(self) weak_self = self;
     self.timer = [NSTimer zj_timerWithTimeInterval:1 repeats:YES addToRunLoopWithMode:NSRunLoopCommonModes block:^(NSTimer * _Nonnull timer) {
         weak_self.index++;
@@ -53,14 +53,29 @@
  设置成UITrackingRunLoopMode时,timer只会在滑动scrollView时触发,其他模式下不触发
  此种情形可以用来记录滑动列表的时间
  */
-- (void)test3 {
+- (void)test2 {
     __weak typeof(self) weak_self = self;
     self.timer = [NSTimer zj_timerWithTimeInterval:1 repeats:YES addToRunLoopWithMode:UITrackingRunLoopMode block:^(NSTimer * _Nonnull timer) {
-        weak_self.index++;
+        weak_self.index++;;
         NSLog(@"执行timer的方法:%@, index = %zd", timer, weak_self.index);
         NSLog(@"currentMode = %@", [NSRunLoop currentRunLoop].currentMode);
     }];
 }
+
+/*
+ ‌NSRunLoopCommonModes‌是指在NSTimer的调度中使用的模式，它等效于NSDefaultRunLoopMode和NSEventTrackingRunLoopMode的结合。这种模式允许定时器在默认模式下运行，同时也能在用户界面事件（如拖动操作）发生时切换到跟踪模式，确保定时器的执行不会受到干扰。
+
+ NSRunLoopCommonModes的作用
+ NSRunLoopCommonModes的主要作用是确保定时器在默认模式下正常运行，同时能够在用户界面事件发生时切换到跟踪模式，从而保证定时器的稳定执行。当使用scheduledTimerWithTimeInterval方法创建定时器时，定时器会被加入到当前线程的Run Loop中，默认模式为NSDefaultRunLoopMode。如果当前线程是主线程（UI线程），某些UI事件（如UIScrollView的拖动操作）会将Run Loop切换到NSEventTrackingRunLoopMode模式。在这个过程中，默认模式中的事件不会被执行，因此定时器也不会执行。为了解决这个问题，可以使用NSRunLoopCommonModes模式，将定时器加入到当前Run Loop中，这样定时器就不会被UI事件干扰‌
+ 1。
+
+ NSRunLoopCommonModes的使用场景
+ NSRunLoopCommonModes的使用场景主要包括：
+
+ ‌‌主线程中的定时器‌：在主线程中创建定时器时，为了避免定时器被UI事件干扰，可以使用NSRunLoopCommonModes模式。
+ ‌‌需要跨模式运行的定时器‌：在某些情况下，定时器需要在不同的模式下运行，NSRunLoopCommonModes可以满足这种需求。
+ 通过使用NSRunLoopCommonModes，可以确保定时器在默认模式下运行，同时也能在需要时切换到跟踪模式，从而提供更稳定和可靠的定时器服务。
+ */
 
 #pragma mark - UITableViewDataSource
 
